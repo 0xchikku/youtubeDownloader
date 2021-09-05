6d0cedbe125dc64f3ab85347d65cb5e686073c40
Ritesh Dubey <0xchikku@gmail.com>
2021-09-05 10:41:25 +0530

    Code Uploaded!

---
+++ main.py
@@ -0,0 +1,24 @@
+from pytube import YouTube
+
+print("YouTube Downloader!\n")
+link = input('Link: ')
+
+try:
+    # YouTube() is a class and it takes the link as argument
+    file = YouTube(link)
+    print(
+        f"Title: {file.title}\nRating: {file.rating}\nDescription: {file.description}")
+    # File is being filtered
+    # available is a list which contains downloadable files
+    available = file.streams.filter(progressive=True, mime_type="video/mp4")
+    # prints the contain in the available
+    for i in available:
+        print(i)
+
+    print(f"Downloading....")
+    # Downloads the last best format of the file
+    available[-1].download()
+    print(f"Downloaded!")
+
+except:
+    print(f"Invalid!")
